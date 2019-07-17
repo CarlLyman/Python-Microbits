fc32064d60757cc96ff9a41da994fa62d172b657
CarlLyman <carl.lyman@outlook.com>
2019-07-17 10:52:48 -0600

    Updated 2.2c to better handle gesture events.
    Updated 2.2c to better handle gesture events.
        # Event - face up?
        faceUp = accelerometer.was_gesture("face up")
        if faceUp:
            display.show(Image.HAPPY)

--- docs/02algorithms/2.2c_Accelerometer-Tilt.py
+++ docs/02algorithms/2.2c_Accelerometer-Tilt.py
@@ -1,6 +1,6 @@
 # 2.2c Accelorator Tilt
 # by C Lyman
-# March 2019
+# July 2019
 # Activity from Module 2 of Coding & Innovation using Microbits - Python
 # This project uses the tilting of Microbit and the accelerometer
 # to display an arrow symbol in the direction of the tilt.
@@ -12,26 +12,37 @@ display.scroll("TILT")
 # forever loop for Events
 while True:
     # Event - face up?
-    if accelerometer.is_gesture("face up"):
+    faceUp = accelerometer.was_gesture("face up")
+    if faceUp:
         display.show(Image.HAPPY)
         
     # Event - face down?
-    if accelerometer.is_gesture("face down"):
+    faceDown = accelerometer.was_gesture("face down")
+    if faceDown:
         display.show(Image.SAD)
 
     # Event - up?
-    if accelerometer.is_gesture("up"):
+    up = accelerometer.was_gesture("up")
+    if up:
         display.show(Image.ARROW_S)
         
     # Event - down?
-    if accelerometer.is_gesture("down"):
+    down = accelerometer.was_gesture("down")
+    if down:
         display.show(Image.ARROW_N)
 
     # Event - tilt right?
-    if accelerometer.is_gesture("right"):
+    right = accelerometer.was_gesture("right")
+    if right:
         display.show(Image.ARROW_E)
  
     # Event - tilt left?
-    if accelerometer.is_gesture("left"):
+    left = accelerometer.was_gesture("left")
+    if left:
         display.show(Image.ARROW_W)
+ 
+    # Event gesture shake
+    shake = accelerometer.was_gesture("shake")
+    if shake:
+        display.scroll("SHK")
         
